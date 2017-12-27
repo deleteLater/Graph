91fd5efde26cbbe88cf64cc2e9994c97f5ba66d9
deleteLater <mikcczhang@qq.com>
2017-12-27 23:21:21 +0800

    correct some mistakes

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+/*图的邻接矩阵存储*/
 class AMGraph {
 public:
 	AMGraph(string vertexes[],int nums) {
@@ -31,7 +32,7 @@ public:
 		ifstream in("data.txt");
 		cin.rdbuf(in.rdbuf());
 		
-		cout << "\t\t - - - 创建图 - - -\n";
+		cout << "\t\t - - - 用邻接矩阵创建图 - - -\n";
 		cout << "结点:";
 		for (int i = 0; i < nums; i++) {
 			cout << vertexes[i] << "  ";
@@ -61,6 +62,8 @@ public:
 			delete[] AdjcencyMatrix[i];
 		}
 		delete[] AdjcencyMatrix;
+		vertexes = nullptr;
+		AdjcencyMatrix = nullptr;
 	}
 	void printAM() {
 		for (int i = 0; i < vertexNums; i++) {
@@ -70,7 +73,7 @@ public:
 			cout << endl;
 		}
 	}
-	void BFSTraversal() {
+	void DFSTraversal() {
 		stack<string> s;
 		bool* visited = new bool[vertexNums];
 		for (int i = 0; i < vertexNums; i++) {
@@ -111,7 +114,7 @@ public:
 		}
 		delete[] visited;
 	}
-	void DFSTraversal() {
+	void BFSTraversal() {
 		queue<string> q;
 		bool* visited = new bool[vertexNums];
 		for (int i = 0; i < vertexNums; i++) {
@@ -154,11 +157,11 @@ public:
 	}
 public:
 	size_t vertexNums;
-	size_t edgeNums;
 private:
 	char **AdjcencyMatrix;
 	string* vertexes;
 	string root;
+private:
 	vector<string> splitString(string str, char delimiter) {
 		string tmp{};
 		vector<string> ret;
@@ -175,11 +178,164 @@ private:
 		return ret;
 	}
 	int locate(string vertex) {
-		int ret = -1;
-		for (int i = 0; i < vertexNums; i++) {
-			if (vertexes[++ret] == vertex)
+		for (int ret = 0; ret < vertexNums; ret++) {
+			if (vertexes[ret] == vertex)
 				return ret;
 		}
+		return -1;
+	}
+};
+
+/*边表节点*/
+class edge_node {
+public:
+	edge_node(size_t index,edge_node* next = nullptr) {
+		this->adjvex_pos = index;
+		this->next = next;
+	}
+	size_t adjvex_pos;
+	edge_node* next;
+};
+/*顶点*/
+class Vertex {
+public:
+	string data;
+	edge_node * next;
+	Vertex(string data, edge_node* next = nullptr) {
+		this->data = data;
+		this->next = next;
+	}
+};
+/*图的邻接表存储*/
+class ALGraph {
+public:
+	ALGraph(Vertex vertexes[], int nums) {
+		//init
+		this->vertexes = vertexes;
+		this->vertexNums = nums;
+		//create
+		streambuf* default_in = cin.rdbuf();
+		ifstream in("data.txt");
+		cin.rdbuf(in.rdbuf());
+
+		cout << "\t\t - - - 用邻接表创建图 - - -\n";
+		cout << "结点:";
+		for (int i = 0; i < nums; i++) {
+			cout << vertexes[i].data << "  ";
+		}
+		cout << endl;
+		for (int i = 0; i < nums; i++) {
+			string line;
+			cout << "输入节点 " << vertexes[i].data << " 的邻接节点(节点间请用空格隔开):";
+			getline(cin, line);
+			cout << line << endl;
+			if (line == "null" || line.empty()) {
+				continue;
+			}
+			//build List for vertexes[i]
+			vector<string> vs = splitString(line, ' ');
+			vertexes[i].next = new edge_node(locate(vs[0]));
+			edge_node* rear = vertexes[i].next;
+			for (int i = 1; i < vs.size(); i++) {
+				rear->next = new edge_node(locate(vs[i]));
+				rear = rear->next;
+			}
+		}
+		cin.rdbuf(default_in);
+	}
+	~ALGraph() {
+		edge_node* n;
+		edge_node* tmp;
+		for (int i = 0; i < vertexNums; i++) {
+			n = vertexes[i].next;
+			while (n) {
+				tmp = n;
+				n = n->next;
+				delete tmp;
+			}
+		}
+		vertexes = nullptr;
+		vertexNums = 0;
+	}
+	void BFSTraversal() {
+		bool* visited = new bool[vertexNums];
+		edge_node* n(nullptr);
+		for (int i = 0; i < vertexNums; i++) {
+			visited[i] = false;
+		}
+		for (int i = 0; i < vertexNums; i++) {
+			if (!visited[i]) {
+				cout << vertexes[i].data << " ";
+				visited[i] = true;
+			}
+			n = vertexes[i].next;
+			while (n) {
+				if (!visited[n->adjvex_pos]) {
+					cout << vertexes[n->adjvex_pos].data << " ";
+					visited[n->adjvex_pos] = true;
+				}
+				n = n->next;
+			}
+		}
+	}
+	void DFSTraversal() {
+		bool* visited = new bool[vertexNums];
+		edge_node* n(nullptr);
+		for (int i = 0; i < vertexNums; i++) {
+			visited[i] = false;
+		}
+		for (int i = 0; i < vertexNums; i++) {
+			if (!visited[i]) {
+				cout << vertexes[i].data << " ";
+				visited[i] = true;
+			}
+			n = vertexes[i].next;
+			while (n) {
+				while (n && !visited[n->adjvex_pos]) {
+					cout << vertexes[n->adjvex_pos].data << " ";
+					visited[n->adjvex_pos] = true;
+					n = vertexes[n->adjvex_pos].next;
+				}
+				if(n) n = n->next;
+			}
+		}
+	}
+	void printAL() {
+		cout << "邻接表如下:\n";
+		edge_node* next;
+		for (int i = 0; i < vertexNums; i++) {
+			cout << vertexes[i].data << ": ";
+			next = vertexes[i].next;
+			while (next) {
+				cout << next->adjvex_pos << "  ";
+				next = next->next;
+			}
+			cout << endl;
+		}
+	}
+	size_t vertexNums;
+private:
+	Vertex * vertexes;
+private:
+	vector<string> splitString(string str, char delimiter) {
+		string tmp{};
+		vector<string> ret;
+		size_t start = 0;
+		size_t end = str.find(delimiter, 0);
+		while (end != string::npos) {
+			ret.push_back(str.substr(start, end - start));
+			start = end + 1;
+			end = str.find(delimiter, start);
+		}
+		if (start != str.length())
+			ret.push_back(str.substr(start));
 		return ret;
+	}
+	int locate(string vex) {
+		for (int ret = 0; ret < vertexNums;ret++) {
+			if (vertexes[ret].data == vex)
+				return ret;
+		}
+		return -1;
 	}
 };
